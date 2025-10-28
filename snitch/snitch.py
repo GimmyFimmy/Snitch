7f55562a1ca2f55c14e705537cfce82e34c75e85
Gimmy_Fimmy <k.pershin2008@gmail.com>
2025-10-28 21:34:34 +0300

    Renamed some classes Optimized imports Updated samples Added class Snitch Removed raise method (to prevent program from crashing)

--- snitch/snitch.py
+++ snitch/snitch.py
@@ -1,12 +1,18 @@
-import enum, asyncio, threading, subprocess, requests
+from subprocess import call
 
-from flask import *
-from nextcord import *
-from nextcord.ext.commands import *
+from threading import Thread
+from asyncio import run_coroutine_threadsafe
 
-CACHE = {}
+from requests import get
+from http import HTTPStatus
+from flask import request, Flask
 
-SAMPLES = {
+from nextcord import Intents, Embed, Color
+from nextcord.ext.commands import Bot
+
+_CACHE = {}
+
+_SAMPLES = {
     "push": {
         "title": "↗ push from {data[pusher][name]}",
         "desc": "Commit Message: {data[head_commit][message]}\n\n`Forced: {data[forced]}`"
@@ -26,47 +32,23 @@ SAMPLES = {
 }
 
 class _Task:
-    def __new__(cls, target: callable) -> threading.Thread:
+    def __new__(cls, target: callable) -> Thread:
         assert callable(target)
 
-        thread = threading.Thread(target=target, daemon=True)
+        thread = Thread(target=target, daemon=True)
         thread.start()
         return thread
 
-
-class ResponseType(enum.Enum):
-    Ok = ["Ok", 200]
-    Invalid = ["Invalid", 400]
-    AccessDenied = ["Access Denied", 403]
-    UnexpectedError = ["Unexpected Error", 500]
-
-class CacheKeyType(enum.Enum):
-    DiscordToken = "DISCORD_TOKEN"
-
-    PushChannelId = "PUSH_CHANNEL_ID"
-    PullRequestChannelId = "PULL_REQUEST_CHANNEL_ID"
-    ReleaseChannelId = "RELEASE_CHANNEL_ID"
-    IssuesChannelId = "ISSUES_CHANNEL_ID"
-
-class ResponseResult:
-    def __new__(cls, response_type: ResponseType, response="") -> Response:
-        assert type(response_type) == ResponseType
-        assert type(response) == str
-
-        return Response(response={response_type.value[0]: response}, status=response_type.value[1])
-
-
-class Request:
+class _Request:
     @staticmethod
     def redirected_url(url: str) -> str:
-        response = requests.get(url, allow_redirects=True)
+        response = get(url, allow_redirects=True)
 
-        if response.status_code == ResponseType.Ok.value[1]:
+        if response.status_code == 200:
             url = response.url
             return url
 
-
-class Receive:
+class _Receive:
     @staticmethod
     def method() -> str:
         return request.method
@@ -80,7 +62,7 @@ class Receive:
         return request.get_json()
 
 
-class Message:
+class _Message:
     def __new__(cls, title: str, desc: str) -> Embed:
         assert type(title) == str
         assert type(desc) == str
@@ -88,9 +70,9 @@ class Message:
         return Embed(title=title, description=desc, color=Color.from_rgb(85, 0, 255))
 
 
-class Convert:
+class _Convert:
     def __new__(cls, header: str, data: dict) -> Embed:
-        header_samples = SAMPLES.get(header)
+        header_samples = _SAMPLES.get(header)
 
         title_sample = header_samples.get("title")
         desc_sample = header_samples.get("desc")
@@ -98,23 +80,23 @@ class Convert:
         title = str.format(title_sample, data=data)
         desc = str.format(desc_sample, data=data)
 
-        return Message(title=title, desc=desc)
+        return _Message(title=title, desc=desc)
 
-class Client:
+class _Client:
     def __init__(self):
         self.intents = Intents.all()
         self.client = Bot(command_prefix="/", intents=self.intents)
 
     def run(self) -> ():
-        token = CACHE.get(CacheKeyType.DiscordToken.value)
+        token = _CACHE.get("BOT_TOKEN")
 
         return self.client.run(token)
 
-    def get_channel(self, header: str) -> Thread:
+    def get_channel(self, header: str):
         header = str.upper(header)
 
         if self.client.is_ready():
-            channel_id = CACHE.get(f"{header}_CHANNEL_ID")
+            channel_id = _CACHE.get(f"{header}_CHANNEL_ID")
 
             if channel_id:
                 channel_id = int(channel_id)
@@ -124,74 +106,68 @@ class Client:
     def send(self, header: str, data: dict) -> ():
         if self.client.is_ready():
             target_channel = self.get_channel(header=header)
-            target_embed = Convert(header=header, data=data)
+            target_embed = _Convert(header=header, data=data)
 
-            return asyncio.run_coroutine_threadsafe(
+            return run_coroutine_threadsafe(
                 target_channel.send(embed=target_embed), self.client.loop
             )
 
-client = Client()
+client = _Client()
 server = Flask(__name__)
 
-@server.route(rule="/", methods=["POST"])
-def webhook():
-    method = Receive.method()
+class Snitch:
+    @staticmethod
+    def __connect_to_server() -> ():
+        try:
+            server.run(port=3000)
+        except Exception as Result:
+            print(f"[!] {Result}")
 
-    if method != "POST":
-        return ResponseResult(response_type=ResponseType.AccessDenied)
+    @staticmethod
+    def __connect_to_client() -> ():
+        try:
+            _Task(target=client.run)
+        except Exception as Result:
+            print(f"[!] {Result}")
 
-    try:
-        data = Receive.json()
-        header = Receive.header()
+    @staticmethod
+    def __connect_to_tunnel() -> ():
+        try:
+            url = _Request.redirected_url("https://smee.io/new")
 
-        if data and header:
-            client.send(header=header, data=data)
-            return ResponseResult(response_type=ResponseType.Ok)
-        else:
-            return ResponseResult(response_type=ResponseType.Invalid)
-    except Exception as Reason:
-        return ResponseResult(response_type=ResponseType.UnexpectedError, response=str(Reason))
-    finally:
-        pass
+            def target():
+                call("npm install --global smee-client", shell=True)
+                call(f"smee -u {url}", shell=True)
 
+            _Task(target=target)
+        except Exception as Result:
+            print(f"[!] {Result}")
 
-def __connect_to_server() -> ():
-    try:
-        server.run(port=3000)
-    except Exception as Result:
-        raise Exception(Result)
+    @staticmethod
+    @server.route(rule="/", methods=["POST"])
+    def __webhook():
+        method = _Receive.method()
 
+        if method != "POST":
+            return HTTPStatus.METHOD_NOT_ALLOWED.value
 
-def __connect_to_client() -> ():
-    try:
-        _Task(target=client.run)
-    except Exception as Result:
-        raise Exception(Result)
+        try:
+            data = _Receive.json()
+            header = _Receive.header()
 
+            if data and header:
+                client.send(header=header, data=data)
+                return HTTPStatus.OK.value
+            else:
+                return HTTPStatus.BAD_REQUEST.value
+        except Exception as Reason:
+            return HTTPStatus.INTERNAL_SERVER_ERROR.value, Reason
 
-def __connect_to_tunnel() -> ():
-    try:
-        url = Request.redirected_url("https://smee.io/new")
+    @staticmethod
+    def run(properties: dict) -> ():
+        for key, value in properties.items():
+            _CACHE[key] = value
 
-        def target():
-            subprocess.call("npm install --global smee-client", shell=True)
-            subprocess.call(f"smee -u {url}", shell=True)
-
-        _Task(target=target)
-    except Exception as Result:
-        raise Exception(Result)
-
-
-def run() -> ():
-    __connect_to_tunnel()
-    __connect_to_client()
-    __connect_to_server()
-
-
-def set_key_value(key: CacheKeyType, value: any) -> ():
-    try:
-        assert type(key) == CacheKeyType
-
-        CACHE[key.value] = value
-    except Exception as Result:
-        raise Exception(Result)
+        Snitch.__connect_to_tunnel()
+        Snitch.__connect_to_client()
+        Snitch.__connect_to_server()
